2479f8034d769f9f4edae30af3c5f9f068b5911d
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2021-09-25 14:35:21 +0700

    fine avg form six number

---
+++ average_of_sic_num.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+main(void){
+    float x1,x2, x3, x4, x5, x6, average;
+    int number;
+    x1 = x2 = x3 = x4 = x5 = x6 = 0.0f;
+    printf("\nEnter up to six numbers terminated with a $ ?");
+    number = scanf("%f%f%f%f%f%f", &x1, &x2, &x3, &x4, &x5, &x6);
+    average = (x1 + x2 + x3 + x4 + x5 + x6) / number;
+    printf("\n Average od %d numbers = %f ", number, average);
+    getch();
+
+}
