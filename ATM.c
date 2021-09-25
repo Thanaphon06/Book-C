9e55b0be76b5cf3dbb5e61f4b5bbd3a731e24ec1
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2021-09-25 14:34:37 +0700

    Emu bank

---
+++ ATM.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "conio.h"
+main()
+{
+    int x,m,b1000,b500,b100;
+    
+    printf("Input : ");
+    scanf("%d", &x);
+    b1000 = x / 1000;
+    m = x % 1000;
+    b500 = m / 500;
+    m = m % 500;
+    b100 = m / 100;
+    printf("b1000 = %d\n", b1000);    
+    printf("b500 = %d\n", b500);    
+    printf("b100 = %d\n", b100);
+    getch()    ;
+}
+
